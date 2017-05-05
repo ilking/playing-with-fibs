1d9039eb5339086840669b4f92282a10d4c637d1
ilking <king.ian.ilk@gmail.com>
2017-05-05 09:15:50 -0400

    Removed code from unrelated project

--- Fib.cs
+++ Fib.cs
@@ -43,33 +43,6 @@ namespace Fibber
       return FibDPHelper(hash, n);
     }
     
-    public static int[,] Dungeon(int[,] dungeon)
-    {
-      int m = dungeon.GetLength(0);
-      int n = dungeon.GetLength(1);
-      
-      int[,] h = new int[m,n];
-      h[m-1,n-1] = Math.Max(1-dungeon[m-1,n-1], 1);
-      
-      for(int i = m-2; i >= 0; i--) {      
-        h[i,n-1] = Math.Max(h[i+1,n-1] - dungeon[i,n-1], 1);
-      }
-      
-      for(int j = n-2; j >= 0; j--){
-        h[m-1,j] = Math.Max(h[m-1,j+1] - dungeon[m-1,j], 1);
-      }
-      
-      for(int i = m-2; i >= 0; i--){
-        for(int j = n-2; j >= 0; j--) {
-          int down = Math.Max(h[i+1,j] - dungeon[i,j], 1);
-          int right = Math.Max(h[i,j+1] - dungeon[i,j], 1);
-          h[i,j] = Math.Min(right, down);
-        }
-      }
-      
-      return h;
-    }
-    
     /* 
         if n is even then k = n/2:
         F(n) = [2*F(k-1) + F(k)]*F(k)
